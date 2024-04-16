92ffaceffe3bc8db845c1f59334e8b02fff58659
Voltury <xxandreas.muellerxx@gmx.net>
2024-04-16 16:22:18 +0200

    Automated obs recording

---
+++ src/EyeTracker/remote/obs_script.py
@@ -0,0 +1,65 @@
+import os
+import time
+from obswebsocket import obsws, requests
+
+
+# All requests:
+# https://github.com/obsproject/obs-websocket/blob/master/src/requesthandler/RequestHandler.cpp
+
+class OBSController:
+    def __init__(self, host, port, password):
+        self.host = host
+        self.port = port
+        self.password = password
+        self.ws = obsws(self.host, self.port, self.password)
+
+    def connect(self):
+        self.ws.connect()
+
+    def disconnect(self):
+        self.ws.disconnect()
+
+    def start_recording(self):
+        self.ws.call(requests.StartRecord())
+
+    def stop_recording(self, participant_id, condition_id):
+        # Check if OBS is currently recording
+        if self.ws.call(requests.GetRecordStatus()).datain['outputActive']:
+            self.ws.call(requests.StopRecord())
+
+            # Wait for OBS to finish writing the file
+            time.sleep(5)
+
+            # Get the current recording path
+            current_path = self.ws.call(requests.GetRecordDirectory()).datain['recordDirectory']
+
+            # Get the latest file in the directory
+            latest_file = max([os.path.join(current_path, f) for f in os.listdir(current_path)], key=os.path.getctime)
+
+            # Define new directory and filename
+            new_dir = f"./../data/{participant_id}/{condition_id}/"
+            file_id = 0
+            new_filename = f"screencapture_{file_id}.mkv"
+            new_path = os.path.join(new_dir, new_filename)
+
+            # Create the new directory if it doesn't exist
+            os.makedirs(new_dir, exist_ok=True)
+
+            # Check if file already exists, if so, increment id until filename is available
+            while os.path.exists(new_path):
+                file_id += 1
+                new_filename = f"screencapture_{file_id}.mkv"
+                new_path = os.path.join(new_dir, new_filename)
+
+            # Move and rename the file in one call
+            os.rename(latest_file, new_path)
+
+
+# Usage
+if __name__ == "__main__":
+    obs = OBSController("localhost", 55558, "CFd0w3bUdQHI3Ctf")
+    obs.connect()
+    obs.start_recording()
+    time.sleep(5)
+    obs.stop_recording(42, 61)
+    obs.disconnect()
